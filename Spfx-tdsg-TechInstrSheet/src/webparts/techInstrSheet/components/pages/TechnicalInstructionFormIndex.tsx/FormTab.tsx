c631f76d80953a334908764ec0385fc2123ada72
SYNOPTEK\sboghara <sboghara@synoptek.com>
2024-12-17 18:36:45 +0530

    -- Update emial templets and form fields as per client feedback

--- Spfx-tdsg-TechInstrSheet/src/webparts/techInstrSheet/components/pages/TechnicalInstructionFormIndex.tsx/FormTab.tsx
+++ Spfx-tdsg-TechInstrSheet/src/webparts/techInstrSheet/components/pages/TechnicalInstructionFormIndex.tsx/FormTab.tsx
@@ -186,7 +186,7 @@ const FormTab: React.FC<any> = ({
                 format="DD-MM-YYYY"
                 placeholder="Select Date"
                 disabledDate={(current) =>
-                  current && current < dayjs().endOf("day")
+                  current && current < dayjs().startOf("day") // Disable past dates
                 }
                 disabled={isViewMode}
               />
@@ -381,7 +381,7 @@ const FormTab: React.FC<any> = ({
                 format="DD-MM-YYYY"
                 placeholder="Select Date"
                 disabledDate={(current) =>
-                  current && current < dayjs().endOf("day")
+                  current && current < dayjs().startOf("day") // Disable past dates
                 }
                 disabled={isViewMode}
               />
