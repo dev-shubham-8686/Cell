81bc914f1a57034ad5b46da1685cc59b038b5c45
SYNOPTEK\sboghara <sboghara@synoptek.com>
2025-01-08 11:59:21 +0530

    -- Add url condition in delegate email for TIS (form/edit)

--- TDSGCellFormatFormsServices/TDSGCellFormat/Implementation/Repository/TechnicalInstructionRepository.cs
+++ TDSGCellFormatFormsServices/TDSGCellFormat/Implementation/Repository/TechnicalInstructionRepository.cs
@@ -2741,7 +2741,7 @@ namespace TDSGCellFormat.Implementation.Repository
                     var technicalData = _context.TechnicalInstructionSheets.Where(x => x.TechnicalId == request.FormId && x.IsDeleted == false).FirstOrDefault();
 
                     var notificationHelper = new NotificationHelper(_context, _cloneContext);
-                    await notificationHelper.DelegateEmail(request.FormId, EmailNotificationAction.delegateUser, request.UserId, request.DelegateUserId, request.activeUserId, technicalData.CTINumber, FormType.MaterialConsumption.ToString(), request.Comments);
+                    await notificationHelper.DelegateEmail(request.FormId, EmailNotificationAction.delegateUser, request.UserId, request.DelegateUserId, request.activeUserId, technicalData.CTINumber, FormType.TechnicalInstruction.ToString(), request.Comments, technicalData.TechnicalId);
 
                     res.StatusCode = Enums.Status.Success;
                     res.Message = Enums.Delegate;
