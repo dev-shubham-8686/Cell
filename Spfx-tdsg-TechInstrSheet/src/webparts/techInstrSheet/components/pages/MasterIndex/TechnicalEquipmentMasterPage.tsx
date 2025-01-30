11ed1b5fdb0659faebc4eed1f24cf148c29f6104
SYNOPTEK\sboghara <sboghara@synoptek.com>
2025-01-30 16:43:03 +0530

    -- Resolved confilct get from develop

--- Spfx-tdsg-TechInstrSheet/src/webparts/techInstrSheet/components/pages/MasterIndex/TechnicalEquipmentMasterPage.tsx
+++ Spfx-tdsg-TechInstrSheet/src/webparts/techInstrSheet/components/pages/MasterIndex/TechnicalEquipmentMasterPage.tsx
@@ -184,8 +184,6 @@ const TechnicalEquipmentMasterPage: React.FC = () => {
         a.EquipmentName.localeCompare(b.EquipmentName),
     },
     {
-<<<<<<< HEAD
-=======
       title: "Created By",
       dataIndex: "UserName",
       key: "UserName",
@@ -199,7 +197,6 @@ const TechnicalEquipmentMasterPage: React.FC = () => {
         a.UpdatedUserName.localeCompare(b.UpdatedUserName),
     },
     {
->>>>>>> c8be2304b24aece2102cb21636eb7e4240453e35
       title: "Is Active",
       dataIndex: "IsActive",
       key: "IsActive",
